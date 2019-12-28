0055695d701acb60dd5ce5190e0576860addbc31
Aruna Fernanda <arufernanda@gmail.com>
2019-12-28 15:49:56 -0300

    rising the rest

--- ruby/exercises.rb
+++ ruby/exercises.rb
@@ -9,3 +9,13 @@ def even_or_odd?(number)
     'odd'
   end
 end
+
+def discount(price, percent_discount)
+  discount = (price * percent_discount) / 100
+  price_with_discount = price - discount
+  if price_with_discount < 9
+    9
+  else
+    price_with_discount
+  end
+end
