8a403cd3cb4a3e39f44d9c4b2f31da355e31ae76
Aruna Fernanda <arufernanda@gmail.com>
2019-12-28 16:08:17 -0300

    finishing exercise 3 in python

--- python/exercises.py
+++ python/exercises.py
@@ -6,3 +6,11 @@ def even_or_odd(num):
     if num % 2 == 0:
         return 'even'
     return 'odd'
+
+
+def discounts(price, percent_discount):
+    discount = (price * percent_discount) / 100
+    price_with_discount = price - discount
+    if price_with_discount < 9:
+        return 9
+    return price_with_discount
