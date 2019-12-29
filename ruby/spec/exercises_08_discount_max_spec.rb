8e8a08f6a1c09d0abc64d0420f05c25107c0308f
Aruna Fernanda <arufernanda@gmail.com>
2019-12-29 17:34:11 -0300

    fixing test code in ruby

--- ruby/spec/exercises_08_discount_max_spec.rb
+++ ruby/spec/exercises_08_discount_max_spec.rb
@@ -1,29 +1,29 @@
 require_relative '../exercises'
 
 RSpec.describe 'discount_max' do
-  context 'with ciscount max for price' do
+  context 'with discount max for price' do
     it 'returns 0 for 11' do
-      expect(installment(11.0)).to eq 0
+      expect(discount_max(11.0)).to eq 0
     end
 
     it 'returns 15 for 13' do
-      expect(installment(13.0)).to eq 15
+      expect(discount_max(13.0)).to eq 15
     end
 
     it 'returns 96 for 300' do
-      expect(installment(300.0)).to eq 96
+      expect(discount_max(300.0)).to eq 96
     end
 
     it 'returns 95 for 257,21' do
-      expect(installment(257.21)).to eq 95
+      expect(discount_max(257.21)).to eq 95
     end
 
     it 'returns 96 for 275' do
-      expect(installment(275.0)).to eq 96
+      expect(discount_max(275.0)).to eq 96
     end
 
     it 'returns 95 for 274,99' do
-      expect(installment(274.99)).to eq 95
+      expect(discount_max(274.99)).to eq 95
     end
   end
 end
